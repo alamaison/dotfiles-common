ee01ef7b4145111bba4ef2fadf188fa0111a21fa
alex <alex@thoughtmachine.net>
2015-12-18 18:56:13 +0000

    Shortcut to visit term.

--- init.el
+++ init.el
@@ -157,6 +157,18 @@
 ;;{{{ Miscellaneous
 
 (global-set-key (kbd "C-c C-c") 'recompile)
+
+(defun visit-term-buffer ()
+  "Create or visit a terminal buffer."
+  (interactive)
+  (if (not (get-buffer "*ansi-term*"))
+      (progn
+        (split-window-sensibly (selected-window))
+        (other-window 1)
+        (ansi-term (getenv "SHELL")))
+    (switch-to-buffer-other-window "*ansi-term*")))
+(global-set-key (kbd "C-x t") 'visit-term-buffer)
+
 (setq-default fill-column 80)
 (prefer-coding-system 'utf-8)
 (setq inhibit-startup-message t)         ; turn off splash screen
