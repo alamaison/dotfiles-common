a39df9e15fc414678596d99363672be76a03279a
Alexander Lamaison <alexander.lamaison@gmail.com>
2015-12-25 22:08:26 +0000

    Irony mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -84,6 +84,27 @@
 (use-package cmake-mode
              :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'"))
 
+(use-package irony
+  :config
+  (setq w32-pipe-read-delay 0)
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  (use-package company-irony
+    :config
+    (eval-after-load 'company
+      '(add-to-list 'company-backends 'company-irony))
+    ;; completion at interesting places, such as after scope operator
+    ;; std::|
+    (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)))
+
 (use-package flycheck
              :config
              (add-hook 'after-init-hook 'global-flycheck-mode))
