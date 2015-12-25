4b1f13681407e3152149f80e335b71dc03a02402
Alexander Lamaison <alexander.lamaison@gmail.com>
2015-12-25 22:05:58 +0000

    Remove legacy magit variable.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,7 +60,6 @@
 (use-package magit
              :ensure t
              :bind ("C-x g" . magit-status)
-             :init (setq magit-last-seen-setup-instructions "1.4.0")
              :config
              (use-package magit-gitflow
                           :config (add-hook
