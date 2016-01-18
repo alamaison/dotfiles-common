1605a683e8921eb8622d94d08214713895f7f125
alex <alex@thoughtmachine.net>
2016-01-18 10:01:21 +0000

    Use protobuf-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -297,6 +297,14 @@
 (setq c-default-style
       '((java-mode . "java") (awk-mode . "awk") (other . "my-bsd")))
 
+(use-package protobuf-mode
+  :config
+  (defconst my-protobuf-style
+    '((c-basic-offset . 4) (indent-tabs-mode . nil)))
+  (add-hook 'protobuf-mode-hook 'subword-mode)
+  (add-hook 'protobuf-mode-hook
+            (lambda () (c-add-style "my-style" my-protobuf-style t))))
+
 ;;{{{ Miscellaneous
 
 (global-set-key (kbd "C-c C-c") 'recompile)
