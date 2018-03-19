ec96acb1e5273ed24973e42e9ed5748c96e06c00
alex <alex@thoughtmachine.net>
2018-03-19 18:00:45 +0000

    User more powerful regex for visual-regex-replace.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -595,7 +595,9 @@
 ;;}}}
 
 (use-package visual-regexp-steroids
+  :ensure
   :config
+  (use-package pcre2el :ensure)
   (define-key global-map (kbd "C-M-%") 'vr/query-replace)
   (define-key esc-map (kbd "C-M-r") 'vr/isearch-backward) ;; C-M-r
   (define-key esc-map (kbd "C-M-s") 'vr/isearch-forward) ;; C-M-s
