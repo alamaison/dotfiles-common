0ce7bd4415e069fb5aed0d1054d41ed4be3d0a6e
alex <alex@thoughtmachine.net>
2018-03-19 18:00:12 +0000

    Java Meghananda.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -322,6 +322,49 @@
               (make-local-variable 'js-indent-level)
               (setq js-indent-level 2))))
 
+(use-package autodisass-java-bytecode
+  :ensure t
+  :defer t)
+
+(use-package google-c-style
+  :defer t
+  :ensure t
+  :commands
+  (google-set-c-style))
+
+(use-package meghanada
+  :defer t
+  :init
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (google-set-c-style)
+              (google-make-newline-indent)
+              (meghanada-mode t)
+              ;; (smartparens-mode t)
+              ;; (rainbow-delimiters-mode t)
+              ;; (highlight-symbol-mode t)
+              (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+  (with-eval-after-load 'company
+    (add-to-list 'company-backends 'company-meghanada))
+  :config
+  (use-package realgud
+    :ensure t)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2)
+  (setq c-basic-offset 2)
+  (setq meghanada-server-remote-debug t)
+  (setq meghanada-javac-xlint "-Xlint:all,-processing")
+  :bind
+  (:map meghanada-mode-map
+        ("C-S-t" . meghanada-switch-testcase)
+        ("M-RET" . meghanada-local-variable)
+        ("C-M-." . helm-imenu)
+        ("M-r" . meghanada-reference)
+        ("M-t" . meghanada-typeinfo)
+        ("C-z" . hydra-meghanada/body))
+  :commands
+  (meghanada-mode))
+
 (use-package scss-mode
   :mode (("\\.scss$" . scss-mode))
   :config (setq scss-compile-at-save nil))
